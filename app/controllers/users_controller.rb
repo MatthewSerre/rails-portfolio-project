20a31a9e77737b99df57967eed5add09aedc3f90
BusterStatus <matthew.serre@gmail.com>
2019-08-05 17:03:12 -0700

    Add create and show methods to users controller and add basic user show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,19 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def create
+        @user = User.create(user_params)
+        redirect_to user_path(@user)
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :password, :password_confirmation)
+    end
+
 end
