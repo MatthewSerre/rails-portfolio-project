43c75ae726c71a1139cc78a0fab039e725d3c7bc
BusterStatus <matthew.serre@gmail.com>
2019-08-05 17:14:48 -0700

    Add new, create, and destroy methods to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,14 @@ class SessionsController < ApplicationController
     end
 
     def create
+        @user = User.find_by(name: params[:user][:name])
+        @user && @user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id
+        redirect_to @user
     end
 
     def destroy
+        session.delete :user_id
     end
-    
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   resources :users
 
-  resources :sessions, only [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
 end
