43a7dd398277fd704737f9554209201174860120
BusterStatus <matthew.serre@gmail.com>
2019-08-06 08:16:57 -0700

    Add create method to events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,4 +7,20 @@ class EventsController < ApplicationController
         @event = Event.new
     end
 
+    def create
+        binding.pry
+        @event = Event.create(event_params)
+        redirect_to event_path(@event)
+    end
+
+    def show
+        @event = Event.find(params[:id])
+    end
+
+    private
+
+    def event_params
+        params.require(:event).permit(:name, :description, :date, :start_time, :end_time, :location, :category)
+    end
+
 end
