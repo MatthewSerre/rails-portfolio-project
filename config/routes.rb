d427d513db66de91ba41d54c4e157f21cb0205e5
BusterStatus <matthew.serre@gmail.com>
2019-08-05 17:33:34 -0700

    Add events controller, routes resources, and views directory

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :events
+
+  root to: "events#index"
+
   resources :users
 
   resources :sessions, only: [:new, :create, :destroy]
+
+
 end
