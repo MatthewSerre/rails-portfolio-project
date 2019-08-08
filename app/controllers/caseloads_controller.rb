1b83747977e6133352deef79dabb9b17635b6a93
BusterStatus <matthew.serre@gmail.com>
2019-08-08 16:54:44 -0700

    Update caseload edit view

--- app/controllers/caseloads_controller.rb
+++ app/controllers/caseloads_controller.rb
@@ -2,6 +2,11 @@ class CaseloadsController < ApplicationController
     def index
     end
 
+    def edit
+        @user = User.find(params[:user_id])
+        @caseload = @user.caseload
+    end
+
     def show
         binding.pry
         @caseload = Caseload.find_by(user_id: params[:user_id])
--- app/models/client.rb
+++ app/models/client.rb
@@ -6,4 +6,8 @@ class Client < ApplicationRecord
     def name
         "#{self.first_name} #{self.last_name}"
     end
+
+    def self.unassigned
+        Client.where("user_id = 0 ")
+    end
 end
