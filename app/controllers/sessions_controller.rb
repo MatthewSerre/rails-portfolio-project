f2363eb889d3a76456362c178d0ea35140596ecb
BusterStatus <matthew.serre@gmail.com>
2019-08-08 08:31:55 -0700

    Fix sessions controller create method to allow for sign in with either native account or Google

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,21 +8,22 @@ class SessionsController < ApplicationController
         end
     end
 
-    # def create
-    #     @user = User.find_by(name: params[:user][:name])
-    #     if @user && @user.authenticate(params[:user][:password])
-    #         session[:user_id] = @user.id
-    #         redirect_to @user
-    #     else
-    #         flash[:error] = "User name or password was incorrect.  Please try again."
-    #         redirect_to new_session_url
-    #     end
-    # end
-
     def create
-        @user = User.find_or_create_from_auth_hash(request.env["omniauth.auth"])
-        session[:user_id] = @user.id
-        redirect_to @user
+        auth = request.env["omniauth.auth"]
+        if !auth.nil?
+            @user = User.find_or_create_from_auth_hash(auth)
+            session[:user_id] = @user.id
+            redirect_to @user
+        else
+            @user = User.find_by(email: params[:user][:email])
+            if @user && @user.authenticate(params[:user][:password])
+                session[:user_id] = @user.id
+                redirect_to @user
+            else
+                flash[:error] = "User name or password was incorrect.  Please try again."
+                redirect_to signin_url
+            end
+        end
     end
 
     def destroy
