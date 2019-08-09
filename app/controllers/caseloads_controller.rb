71036af878c34e2db6b644f8d5e5b1662effa9a6
BusterStatus <matthew.serre@gmail.com>
2019-08-09 12:43:37 -0700

    Fix caseload controller edit and update methods

--- app/controllers/caseloads_controller.rb
+++ app/controllers/caseloads_controller.rb
@@ -7,6 +7,16 @@ class CaseloadsController < ApplicationController
         @caseload = @user.caseload
     end
 
+    def update
+        binding.pry
+        @user = User.find(params[:user_id])
+        @caseload = @user.caseload
+        client = Client.find(params[:caseload][:client_ids])
+        client.caseload_id = @caseload.id
+        client.save
+        redirect_to user_caseload_url(@user, @caseload)
+    end
+
     def show
         @caseload = Caseload.find_by(user_id: params[:user_id])
     end
